85d494d301beca0ce8363d91d3dc1d5e2859e517
Andrew Chai <magerate1129@gmail.com>
2012-12-08 20:49:18 +0800

    chaigoo startup

---
+++ Chaigoo/Geometries/Vector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Chaigoo.Geometries
+{
+    public struct Vector:IEquatable<Vector>
+    {
+        public static readonly Vector Zero = new Vector(.0f,.0f);
+        public float X, Y;
+
+        public Vector(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void 
+    }
+}
